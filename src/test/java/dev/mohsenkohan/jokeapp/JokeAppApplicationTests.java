81ff84078a8281dfc9ea549f40366238878ce39c
Mohsen Kohan <mohsenkohan.dev@gmail.com>
2020-03-21 16:44:50 +0430

    Initial Project Commit

---
+++ src/test/java/dev/mohsenkohan/jokeapp/JokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mohsenkohan.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
